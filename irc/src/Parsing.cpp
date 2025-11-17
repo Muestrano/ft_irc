13970f7bd7985cb1e32bd60e4d417360067625c2
Pierre Carlier <picarlie@c1r3p7.42perpignan.fr>
2025-11-17 18:17:08 +0100

    Ca commence !

--- irc/src/Parsing.cpp
+++ irc/src/Parsing.cpp
@@ -25,3 +25,26 @@ Command:
 	
 #
 */
+
+
+/*
+
+/// MESSAGE PROCESSING ///
+void Serv::processMessage(int user_fd, const char *message)
+{
+	std::string msg(message);
+	std::string buffer;
+
+	this->clients[user_fd]->appendToBuffer(msg);
+	buffer = this->clients[user_fd]->getBuffer();
+	if (buffer.find_first_of("\r\n") == std::string::npos)
+		return;
+
+	std::vector<std::string> commands = splitCommands(buffer);
+	for (std::vector<std::string>::iterator it = commands.begin(); it != commands.end(); ++it)
+		this->interpret_message(user_fd, *it);
+	if (this->clients[user_fd])
+		this->clients[user_fd]->clearBuffer();
+}
+
+*/
